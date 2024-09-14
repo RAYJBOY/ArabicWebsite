f3453588a71185f01dbaef4201e50b8fcf96fed4
Hamza <muhammadhamzaimam786@gmail.com>
2024-09-14 18:41:33 +0100

    added login component and deleted toolpad/core

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 import { SideBar } from "./components/sidebar/SideBar";
 import { Header } from "./components/header/Header";
+import { Login } from "./components/login/Login";
 
 function App() {
+
+  const handleClose = () => {
+
+  }
+
   return (
     <div>
       <Header />
       <SideBar />
+      <Login open={false} handleClose={handleClose}/>
     </div>
   );
 }
