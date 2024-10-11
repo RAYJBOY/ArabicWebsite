2776587de937abaff71e41d10dc3e016302e26c9
Hamza <muhammadhamzaimam786@gmail.com>
2024-10-11 19:30:14 +0100

    started enrollment

---
+++ client/src/components/enrollment/CourseSelect.tsx
@@ -0,0 +1,62 @@
+import {
+  FormControl,
+  InputLabel,
+  Select,
+  ListSubheader,
+  MenuItem,
+  SelectChangeEvent,
+} from "@mui/material";
+import { useState } from "react";
+
+export const CourseSelect = () => {
+  const [selectedCourse, setSelectedCourse] = useState("");
+
+  const handleCourseSelect = (course: SelectChangeEvent) => {
+    setSelectedCourse(course.target.value);
+  };
+
+  return (
+    <>
+      <FormControl fullWidth>
+        <InputLabel required id="courseSelect">
+          Course
+        </InputLabel>
+        <Select
+          value={selectedCourse}
+          label="Course"
+          labelId="courseSelect"
+          onChange={handleCourseSelect}
+          MenuProps={{
+            PaperProps: {
+              style: {
+                maxHeight: 500,
+              },
+            },
+            marginThreshold: 0,
+          }}
+        >
+          <ListSubheader>Arabic</ListSubheader>
+          <MenuItem value="Beginner">Beginner</MenuItem>
+          <MenuItem value="Advanced">Advanced</MenuItem>
+          <MenuItem value="Quranic Arabic Words">Quranic Arabic Words</MenuItem>
+          <MenuItem value="Conversation">Conversation</MenuItem>
+          <MenuItem value="Level 1">Level 1</MenuItem>
+          <MenuItem value="Level 2">Level 2</MenuItem>
+          <MenuItem value="Level 3">Level 3</MenuItem>
+          <ListSubheader>Islamic Studies</ListSubheader>
+          <MenuItem value="Fiqh">Fiqh</MenuItem>
+          <MenuItem value="Seera">Seera</MenuItem>
+          <MenuItem value="Hadeeth">Hadeeth</MenuItem>
+          <MenuItem value="Aqeeda">Aqeeda</MenuItem>
+          <MenuItem value="Tafseer">Tafseer</MenuItem>
+          <MenuItem value="Islamic History">Islamic History</MenuItem>
+          <ListSubheader>Quran</ListSubheader>
+          <MenuItem value="How To Read">How To Read</MenuItem>
+          <MenuItem value="Recitation">Recitation</MenuItem>
+          <MenuItem value="Memorization">Memorization</MenuItem>
+          <MenuItem value="Tajweed">Tajweed</MenuItem>
+        </Select>
+      </FormControl>
+    </>
+  );
+};
--- client/src/pages/EnrollmentPage.tsx
+++ client/src/pages/EnrollmentPage.tsx
@@ -1,54 +1,29 @@
-import { FormControl, InputLabel, ListSubheader, MenuItem, Select, SelectChangeEvent, Typography } from "@mui/material"
+import { Grid2, TextField, Typography } from "@mui/material"
 import { Header } from "../components/header/Header"
 import "./EnrollmentPage.css"
-import { useState } from "react"
+import { CourseSelect } from "../components/enrollment/CourseSelect"
 
 export const EnrollmentPage = () => {
-
-    const[selectedCourse, setSelectedCourse] = useState("");
-
-    const handleCourseSelect = (course: SelectChangeEvent) => {
-        setSelectedCourse(course.target.value);
-    }
-
+    
     return (
         <>
             <Header displayTitle={true}/>
             <div style={{margin: '2%'}}>
                 <Typography variant="h2" align="center">Enrollment</Typography>
             </div>
-            <div className="bookingContainer">
-                <FormControl size="small" sx={{ width: "100%" }}>
-                    <InputLabel>Course</InputLabel>
-                    <Select
-                        value={selectedCourse}
-                        label="Course"
-                        onChange={handleCourseSelect}
-                        fullWidth
-                        MenuProps={{
-                            style: {
-                                maxHeight: 500,
-                            }
-                        }}
-                    >
-                        <ListSubheader>Arabic</ListSubheader>
-                        <MenuItem value="Beginner">Beginner</MenuItem>
-                        <MenuItem value="Advanced">Advanced</MenuItem>
-                        <MenuItem value="Quranic Arabic Words">Quranic Arabic Words</MenuItem>
-                        <MenuItem value="Conversation">Conversation</MenuItem>
-                        <MenuItem value="Level 1">Level 1</MenuItem>
-                        <MenuItem value="Level 2">Level 2</MenuItem>
-                        <MenuItem value="Level 3">Level 3</MenuItem>
-                        <ListSubheader>Arabic</ListSubheader>
-                        <MenuItem value="">Ten</MenuItem>
-                        <MenuItem value="">Twenty</MenuItem>
-                        <MenuItem value="">Thirty</MenuItem>
-                        <ListSubheader>Arabic</ListSubheader>
-                        <MenuItem value="">Ten</MenuItem>
-                        <MenuItem value="">Twenty</MenuItem>
-                        <MenuItem value="">Thirty</MenuItem>
-                    </Select>
-                </FormControl>
+            <div>
+                <Grid2 container spacing={2}>
+                    <Grid2 size={4}>
+                        <TextField variant="outlined" label="First Name" fullWidth required/>
+                    </Grid2>
+                    <Grid2 size={4}>
+                        <TextField variant="outlined" label="Last Name" fullWidth required/>
+                    </Grid2>
+                    <Grid2 size={4}>
+                        <CourseSelect/>
+                    </Grid2>
+
+                </Grid2>
             </div>
         </>
     )
