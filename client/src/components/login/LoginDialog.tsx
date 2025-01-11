f6747689040a9e72906d5a2d96926bb7cb72858f
Hamza <muhammadhamzaimam786@gmail.com>
2025-01-11 15:50:17 +0000

    aded axios, set up first endpoint from fe to be to db, setup db tables

--- client/src/components/login/LoginDialog.tsx
+++ client/src/components/login/LoginDialog.tsx
@@ -1,16 +1,43 @@
 import React, { useState } from "react";
-import { Dialog, DialogTitle, DialogContent, Button, TextField, Box } from "@mui/material";
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  Button,
+  TextField,
+  Box,
+} from "@mui/material";
+import axios from "axios";
 
 type LoginModalProps = {
   open: boolean;
   handleClose: () => void;
 };
 
-export const LoginDialog: React.FC<LoginModalProps> = ({ open, handleClose }) => {
-  const [isSignUp, setIsSignUp] = useState(false);
+export const LoginDialog: React.FC<LoginModalProps> = ({
+  open,
+  handleClose,
+}) => {
+  const [isSignUp, setIsSignUp] = useState<boolean>(false);
+  const [username, setUsername] = useState<string>();
+  const [email, setEmail] = useState<string>();
+  const [password, setPassword] = useState<string>();
 
   const switchMode = () => {
     setIsSignUp((prevIsSignUp) => !prevIsSignUp);
+  };
+
+  const handleSignUp = async () => {
+    console.log("Username: ", username);
+    console.log("Email: ", email);
+    console.log("Password: ", password);
+    axios("http://localhost:5001/sign-up", {
+        method: 'POST',
+        headers: { "Content-Type": "application/json" },
+        data: JSON.stringify({ username, email, password }),
+      })
+      .then((response) => console.log(response))
+      .catch((err) => console.log(err));
   };
 
   return (
@@ -23,34 +50,41 @@ export const LoginDialog: React.FC<LoginModalProps> = ({ open, handleClose }) =>
           variant="outlined"
           fullWidth
           margin="normal"
+          onChange={(input: React.ChangeEvent<HTMLInputElement>) =>
+            setUsername(input.target.value)
+          }
         />
         {isSignUp && (
           <TextField
-          required type="email"
-          label="Email"
-          variant="outlined"
-          fullWidth
-          margin="normal"
-        />
+            required
+            type="email"
+            label="Email"
+            variant="outlined"
+            fullWidth
+            margin="normal"
+            onChange={(input: React.ChangeEvent<HTMLInputElement>) =>
+              setEmail(input.target.value)
+            }
+          />
         )}
         <TextField
-          required type="password"
+          required
+          type="password"
           label="Password"
           variant="outlined"
           fullWidth
           margin="normal"
+          onChange={(input: React.ChangeEvent<HTMLInputElement>) =>
+            setPassword(input.target.value)
+          }
         />
-        {isSignUp && (
-          <TextField 
-            required type="password"
-            label="Confirm Password"
-            variant="outlined"
-            fullWidth
-            margin="normal"
-          />
-        )}
         <Box sx={{ mt: 2 }}>
-          <Button variant="contained" color="primary" fullWidth>
+          <Button
+            variant="contained"
+            color="primary"
+            fullWidth
+            onClick={handleSignUp}
+          >
             {isSignUp ? "Sign Up" : "Sign In"}
           </Button>
           {!isSignUp && (
@@ -59,13 +93,15 @@ export const LoginDialog: React.FC<LoginModalProps> = ({ open, handleClose }) =>
               fullWidth
               onClick={() => console.log("Reset Password Clicked")}
             >
-                Reset Password
+              Reset Password
             </Button>
           )}
         </Box>
         <Box sx={{ mt: 2 }}>
           <Button onClick={switchMode} fullWidth>
-            {isSignUp ? "Already have an account? Sign In" : "Don't have an account? Sign Up"}
+            {isSignUp
+              ? "Already have an account? Sign In"
+              : "Don't have an account? Sign Up"}
           </Button>
         </Box>
       </DialogContent>
--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,30 @@
-import express, { Express, Request, Response , Application } from 'express';
+import { PrismaClient } from '@prisma/client';
+import bodyParser from 'body-parser';
+import express, { Request, Response , Application } from 'express';
 
 const app: Application = express();
+const prisma = new PrismaClient();
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Welcome to Express & TypeScript Server');
+var jsonParser = bodyParser.json();
+const cors = require('cors');
+
+app.use(cors({
+    origin: 'http://localhost:3000', // Replace with your frontend URL
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  }));
+
+app.get('/', async (req: Request, res: Response) => {
+    const allUsers = await prisma.user.findMany();
+    res.json(allUsers);
 });
 
-app.listen(5000, () => console.log("Listening on port 5000"));
+app.post('/sign-up', jsonParser, async (req: Request, res: Response) => {
+    console.log("Received POST request:", req.method, req.url);
+    console.log("Request body:", req.body);
+    const newUser = await prisma.user.create({data: req.body});
+    console.log("Created new user:", newUser);
+    res.status(200).json(newUser);
+});
+
+app.listen(5001, () => console.log("Listening on port 5001"));
