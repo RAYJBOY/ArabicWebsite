53e53999ab8c8abacaa7e492d0ee327530634db2
Hamza <muhammadhamzaimam786@gmail.com>
2025-07-08 19:25:46 +0100

    made contact us page mobile responsive

--- client/src/pages/ContactUsPage.tsx
+++ client/src/pages/ContactUsPage.tsx
@@ -1,31 +1,31 @@
-import { Typography } from "@mui/material";
+import { Box, Grid2, Typography } from "@mui/material";
 import { ContactUsForm } from "../components/contact/ContactUsForm";
 
 export const ContactUsPage = () => {
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "row",
-        alignItems: "center",
-        marginTop: "8%",
-        gap: "10%",
-        justifyContent: "center",
-      }}
-    >
-      <div>
-        <Typography variant="h2" gutterBottom align="center">
-          {" "}
-          Contact Us{" "}
-        </Typography>
-        <Typography variant="h6" gutterBottom align="center">
-          {" "}
-          Have questions? We are here to help!{" "}
-        </Typography>
-      </div>
-      <div>
-        <ContactUsForm />
-      </div>
-    </div>
+    <Box sx={{ mt: 8, px: 2 }}>
+      <Grid2
+        container
+        spacing={4}
+        justifyContent="center"
+        alignItems="center"
+        direction={{ xs: "column", md: "row" }}
+      >
+        <Grid2 size={{ xs: 12, md: 5 }}>
+          <Box textAlign="center">
+            <Typography variant="h2" gutterBottom>
+              Contact Us
+            </Typography>
+            <Typography variant="h6" gutterBottom>
+              Have questions? We are here to help!
+            </Typography>
+          </Box>
+        </Grid2>
+
+        <Grid2 size={{ xs: 12, md: 5 }}>
+          <ContactUsForm />
+        </Grid2>
+      </Grid2>
+    </Box>
   );
 };
