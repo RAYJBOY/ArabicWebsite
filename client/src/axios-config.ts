87235b3e1e840476ec44c1a443a2f8e6217f89fe
Hamza <muhammadhamzaimam786@gmail.com>
2025-07-10 20:51:44 +0100

    fix axios

--- client/src/axios-config.ts
+++ client/src/axios-config.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: `${process.env.REACT_APP_BACKEND_URL}:${process.env.REACT_APP_BACKEND_PORT}`,
+  baseURL: `${process.env.REACT_APP_BACKEND_URL}`,
   withCredentials: true,
 });
 
