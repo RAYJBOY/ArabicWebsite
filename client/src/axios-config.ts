21d8b218b6596dee15dfee388bef7eee4adfe66f
Hamza <muhammadhamzaimam786@gmail.com>
2025-07-10 20:00:43 +0100

    configured base url for backend

--- client/src/axios-config.ts
+++ client/src/axios-config.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: `${process.env.REACT_APP_BACKEND_URL}:${process.env.REACT_APP_BACKEND_PORT}`,
   withCredentials: true,
 });
 
