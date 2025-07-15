bb0cc8d899a89fb0ed0fbf10cf7a026ae83509ea
Hamza <muhammadhamzaimam786@gmail.com>
2025-07-15 19:04:46 +0100

    fixed cookie issues

--- server/src/services/user/loginUser.ts
+++ server/src/services/user/loginUser.ts
@@ -64,15 +64,15 @@ export const loginUser = async (
 
     res.cookie("access_token", accessToken, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "none",
+      secure: process.env.NODE_ENV === "production" ? true : false,
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
       maxAge: 15 * 60 * 1000, // 15 mins
     });
 
     res.cookie("refresh_token", refreshToken, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "none",
+      secure: process.env.NODE_ENV === "production" ? true : false,
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
       maxAge: 30 * 1000 * 60 * 60 * 24, // 30 days
     });
 
